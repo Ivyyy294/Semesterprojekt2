0df6a1a59882a2160bbef8f9a03db550c96a83e4
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-06-08 08:25:56 +0200

    New Camera ChatTerminal

--- Assets/Scripts/ChatTerminal/ChatTerminalObj.cs
+++ Assets/Scripts/ChatTerminal/ChatTerminalObj.cs
@@ -2,13 +2,127 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ivyyy.GameEvent;
+using Cinemachine;
+using Ivyyy.StateMachine;
+
+public class OfflineState : IState
+{
+	ChatTerminalObj terminalObj;
+
+	public void Enter (GameObject obj)
+	{
+		terminalObj = obj.GetComponent<ChatTerminalObj>();
+		terminalObj.terminalCamera.Priority = 0;
+		terminalObj.eventReleasePlayer?.Raise();
+	}
+
+	public void Update (GameObject obj)
+	{
+		if (terminalObj.active)
+			terminalObj.SetState (ChatTerminalObj.powerUpState);
+	}
+}
+
+public class PowerUpState : IState
+{
+	ChatTerminalObj terminalObj;
+	float timer = 0f;
+
+	public void Enter (GameObject obj)
+	{
+		terminalObj = obj.GetComponent<ChatTerminalObj>();
+		terminalObj.eventLockPlayer?.Raise();
+		terminalObj.terminalCamera.Priority = 2;
+		timer = 0f;
+	}
+
+	public void Update (GameObject obj)
+	{
+		if (timer >= terminalObj.minTransitionTime && !terminalObj.cinemachineBrain.IsBlending)
+			terminalObj.SetState (ChatTerminalObj.onlineState);
+		else
+			timer += Time.deltaTime;
+	}
+}
+
+public class PowerDownState : IState
+{
+	ChatTerminalObj terminalObj;
+	float timer = 0f;
+
+	public void Enter (GameObject obj)
+	{
+		terminalObj = obj.GetComponent<ChatTerminalObj>();
+		terminalObj.terminalCamera.Priority = 0;
+		terminalObj.terminalUI.SetActive (false);
+		timer = 0f;
+	}
+
+	public void Update (GameObject obj)
+	{
+		if (timer >= terminalObj.minTransitionTime && !terminalObj.cinemachineBrain.IsBlending)
+			terminalObj.SetState (ChatTerminalObj.offlineState);
+		else
+			timer += Time.deltaTime;
+	}
+}
+
+public class OnlineState : IState
+{
+	ChatTerminalObj terminalObj;
+
+	public void Enter (GameObject obj)
+	{
+		terminalObj = obj.GetComponent<ChatTerminalObj>();
+		terminalObj.terminalUI.SetActive (true);
+	}
+
+	public void Update (GameObject obj)
+	{
+		if (!terminalObj.active)
+			terminalObj.SetState (ChatTerminalObj.powerDownState);
+	}
+}
 
 public class ChatTerminalObj : MonoBehaviour, Ivyyy.Interfaces.InteractableObject
 {
-	[SerializeField] GameEvent gameEvent;
+	[Header ("Lara Values")]
+	public GameEvent eventLockPlayer;
+	public GameEvent eventReleasePlayer;
+	public float minTransitionTime = 1f;
+	[Space()]
+	public CinemachineVirtualCamera terminalCamera;
+	public CinemachineBrain cinemachineBrain;
+	[Space()]
+	public GameObject terminalUI;
+
+	public bool active;
+	public static OfflineState offlineState = new OfflineState();
+	public static PowerUpState powerUpState = new PowerUpState();
+	public static OnlineState onlineState = new OnlineState();
+	public static PowerDownState powerDownState = new PowerDownState();
+
+	private IState currentState;
+
+	private void Start()
+	{
+		SetState (new OfflineState());
+	}
+
+	public void Update()
+	{
+		currentState?.Update (gameObject);
+	}
+
+	public void SetState (IState newState)
+	{
+		currentState = newState;
+		currentState.Enter(gameObject);
+	}
+
 
 	public void Interact()
 	{
-		gameEvent?.Raise();
+		active = !active;
 	}
 }
