a770f8bc00b4483772979d87d693b7e230ef94fe
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-06-14 15:26:57 +0200

    NEW AudioAsset

---
+++ Assets/Scripts/AudioSystem/AudioAsset.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "NewAudioAsset", menuName = "AudioAsset")]
+public class AudioAsset : ScriptableObject
+{
+	public enum PlayStyle
+	{
+		RANDOM,
+		IN_ORDER,
+		REVERSE
+	}
+
+    public AudioClip[] audioClips;
+	[HideInInspector] public Vector2 volume = new Vector2 (0.5f, 0.5f);
+	[HideInInspector] public Vector2 pitch = new Vector2 (1f, 1f);
+	[SerializeField] PlayStyle playStyle = PlayStyle.RANDOM;
+
+	private Stack <AudioClip> clipBuffer = new Stack <AudioClip>();
+	private PlayStyle oldPlayStyle;
+
+#if UNITY_EDITOR
+	private AudioSource previewer;
+
+	private void OnEnable()
+	{
+		previewer = EditorUtility.CreateGameObjectWithHideFlags ("AudioPreview", HideFlags.HideAndDontSave, typeof (AudioSource)).GetComponent <AudioSource>();
+	}
+
+	private void OnDisable()
+	{
+		DestroyImmediate (previewer.gameObject);
+	}
+
+	public void PlayPreview()
+	{
+		Play (previewer);
+	}
+
+	public void StopPreview()
+	{
+		previewer.Stop();
+	}
+#endif
+
+	public void Play (AudioSource audioSource = null)
+	{
+		if (audioClips.Length > 0)
+		{
+			if (clipBuffer.Count == 0 || oldPlayStyle != playStyle)
+				ShuffleAudioClips();
+
+			AudioSource source = audioSource;
+
+			if (source == null)
+			{
+				var obj = new GameObject ("Sound", typeof (AudioSource));
+				source = obj.GetComponent <AudioSource>();
+			}
+
+			source.clip = clipBuffer.Pop();
+			source.volume = Random.Range (volume.x, volume.y);
+			source.pitch = Random.Range (pitch.x, pitch.y);
+			source.Play();
+
+#if UNITY_EDITOR
+			if (audioSource == previewer)
+				return;
+#endif
+
+			Destroy (source.gameObject, source.clip.length / source.pitch);
+		}
+	}
+
+	private void ShuffleAudioClips()
+	{
+		clipBuffer.Clear();
+
+		if (playStyle == PlayStyle.RANDOM)
+		{
+			while (clipBuffer.Count < audioClips.Length)
+			{
+				int index = Random.Range (0, audioClips.Length);
+
+				if (!clipBuffer.Contains (audioClips[index]))
+					clipBuffer.Push (audioClips[index]);
+			}
+		}
+		else if (playStyle == PlayStyle.REVERSE)
+		{
+			foreach (AudioClip i in audioClips)
+				clipBuffer.Push (i);
+		}
+		else
+		{
+			for (int i = audioClips.Length - 1; i >= 0; --i)
+				clipBuffer.Push (audioClips[i]);
+		}
+
+		oldPlayStyle = playStyle;
+	}
+}
---
+++ Assets/Scripts/AudioSystem/Editor/AudioAssetEditor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AudioAsset))]
+public class AudioAssetEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		//Volume
+		EditorGUILayout.Space();
+		AudioAsset audioAsset = (AudioAsset)target;
+		string labelVolume = ("Volume \t[" + audioAsset.volume.x.ToString("0.00") + " - " + audioAsset.volume.y.ToString("0.00") + "]");
+		EditorGUILayout.MinMaxSlider (labelVolume, ref audioAsset.volume.x, ref audioAsset.volume.y, 0f, 1f);
+		
+		//Pitch
+		EditorGUILayout.Space();
+		string labelPitch = ("Pitch \t [" + audioAsset.pitch.x.ToString("0.00") + " - " + audioAsset.pitch.y.ToString("0.00") + "]");
+		EditorGUILayout.MinMaxSlider (labelPitch, ref audioAsset.pitch.x, ref audioAsset.pitch.y, 0f, 2f);
+
+		EditorGUILayout.Space();
+
+		if (GUILayout.Button("Play Preview"))
+			audioAsset.PlayPreview();
+
+		EditorGUILayout.Space();
+
+		if (GUILayout.Button("Stop Preview"))
+			audioAsset.StopPreview();
+	}
+
+	private Texture2D CreateTextureWithColor(Color color)
+    {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, color);
+        texture.Apply();
+        return texture;
+    }
+}
