a6d38625ecf9aaa070f7d8942522dcca314c9fc3
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-06-22 11:53:29 +0200

    New Kitchen Animations

---
+++ Assets/Scripts/Other/Kitchen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ivyyy.Interfaces;
+
+public class Kitchen : MonoBehaviour, InteractableObject
+{
+	bool active = false;
+	Animator animator;
+
+	public void Interact()
+	{
+		active = !active;
+
+		if (active)
+			animator?.SetTrigger ("Open");
+		else
+			animator?.SetTrigger ("Close");
+	}
+
+	private void Start()
+	{
+		animator = GetComponent <Animator>();
+	}
+}
