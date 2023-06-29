db95db6666b8bf507b6ef929678bb8f2c04df5e8
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-06-29 22:43:10 +0200

    CHG DoorTerminal

--- Assets/Scripts/RoomObjects/DoorTerminal.cs
+++ Assets/Scripts/RoomObjects/DoorTerminal.cs
@@ -2,30 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ivyyy.Interfaces;
-public class DoorTerminal : AudioPlayer, InteractableObject
+using Ivyyy.StateMachine;
+
+[RequireComponent (typeof (AudioPlayer))]
+public class DoorTerminal : PushdownAutomata, InteractableObject
 {
+	public class BaseState : IState
+	{
+		protected DoorTerminal doorTerminal;
+		public virtual void Enter (GameObject obj)
+		{
+			doorTerminal = obj.GetComponent<DoorTerminal>();
+		}
+
+		public virtual void Update (GameObject obj)
+		{
+		}
+
+		public virtual void Exit(GameObject obj)
+		{
+		}
+	}
+
+	[System.Serializable]
+	public class AccessDeniedState : BaseState
+	{
+		[SerializeField] AudioPlayer audioPlayer;
+		[SerializeField] AudioAsset audioAsset;
+
+		public override void Enter (GameObject obj)
+		{
+			base.Enter (obj);
+			audioPlayer?.Play(audioAsset);
+		}
+
+		public override void Update (GameObject obj)
+		{
+			if (!audioPlayer.IsPlaying())
+				doorTerminal.PopState();
+		}
+	}
+
+	[System.Serializable]
+	public class PuckState : BaseState
+	{
+		[SerializeField] AudioPlayer audioPlayer;
+		[SerializeField] List <AudioAsset> audioAssets;
+		int counter;
+		bool audioPlayed;
+
+		public override void Enter (GameObject obj)
+		{
+			base.Enter (obj);
+			audioPlayed = false;
+		}
+
+		public override void Update (GameObject obj)
+		{
+			if (audioAssets.Count > 0 && ! audioPlayed)
+			{
+				audioPlayer?.Play (audioAssets[counter]);
+				audioPlayed = true;
+			} else if (!audioPlayer.IsPlaying())
+				doorTerminal.PopState();
+		}
+
+		public override void Exit(GameObject obj)
+		{
+			counter++;
+			counter = Mathf.Clamp (counter, 0, audioAssets.Count - 1);
+		}
+	}
+
+	public BaseState baseState = new BaseState();
+	public AccessDeniedState accessDenied = new AccessDeniedState();
+	public PuckState puckState = new PuckState();
+
 	public bool active;
-	[SerializeField] float coolDown = 0.5f;
-	float timer = 0f;
 	public bool locked = true;
 
     public void Interact()
 	{
-		if (locked && timer >= coolDown)
+		if (locked)
 		{
 			active = false;
-			Play();
-			timer = 0f;
+			PushState (puckState);
+			PushState (accessDenied);
 		}
-		else if (!locked)
-		{
+		else
 			active = true;
-		}
 	}
 
-	private void Update()
+	private void Start()
 	{
-		if (timer < coolDown)
-			timer += Time.deltaTime;
+		PushState (baseState);
 	}
 }
