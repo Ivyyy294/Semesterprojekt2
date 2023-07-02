b84f9dde6fddd4b5e603991be53c49c4c16336fe
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-07-02 14:32:43 +0200

    CHG CryoDay3Event

--- Assets/Scripts/CustomEvents/CryoDay3Event.cs
+++ Assets/Scripts/CustomEvents/CryoDay3Event.cs
@@ -2,32 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Ivyyy.GameEvent;
+using Ivyyy.StateMachine;
 
 [RequireComponent (typeof (AudioPlayer))]
-class CryoDay3Event : MonoBehaviour
+class CryoDay3Event : FiniteStateMachine
 {
-	[SerializeField] Door cryoDoor;
-	[SerializeField] InteractableCamera cryoChair;
-	[SerializeField] Room room;
-	[SerializeField] List <AudioAsset> voiceLines;
-	[SerializeField] AudioAsset voiceLinesTrapped;
-	[SerializeField] BlackBoardProperty testProperty;
-	[SerializeField] ChatTerminalObj chatTerminal;
-	[SerializeField] GameEvent closeTerminal;
+	public class BaseState : IState
+	{
+		protected CryoDay3Event cryoEvent;
+		public virtual void Enter (GameObject obj)
+		{
+			cryoEvent = obj.GetComponent <CryoDay3Event>();
+		}
+		public virtual void Update (GameObject obj){}
+		public virtual void Exit(GameObject obj) {}
+	}
 
+	[System.Serializable]
+	public class Stage1 : BaseState
+	{
+		[SerializeField] Door cryoDoor;
+		[SerializeField] List <AudioAsset> voiceLines;
+		[SerializeField] ChatTerminalObj chatTerminal;
+		[SerializeField] GameEvent closeTerminal;
+		int currentIndex;
+		
+		public override void Enter(GameObject obj)
+		{
+			base.Enter(obj);
+			currentIndex = 0;
+			cryoDoor.open = true;
+			chatTerminal.SetLocked(true);
+			closeTerminal.Raise();
+		}
+
+		public override void Update(GameObject obj)
+		{
+			if (!cryoEvent.playerInCryoRoom)
+			{
+				if (currentIndex >= voiceLines.Count && !cryoEvent.audioPlayer.IsPlaying())
+					cryoEvent.EnterState (cryoEvent.idleState);
+				else if (!cryoEvent.audioPlayer.IsPlaying())
+					cryoEvent.audioPlayer.Play(voiceLines[currentIndex++]);
+			}
+			else
+				cryoEvent.EnterState (cryoEvent.stage2);
+		}
+
+		public override void Exit(GameObject obj)
+		{
+			cryoDoor.open = false;
+			chatTerminal.SetLocked(false);
+		}
+	}
+
+	[System.Serializable]
+	public class Stage2 : BaseState
+	{
+		[SerializeField] Door cryoDoor;
+		[SerializeField] InteractableCamera cryoChair;
+		[SerializeField] Room room;
+		[SerializeField] AudioAsset voiceLinesTrapped;
+		[SerializeField] BlackBoardProperty testProperty;
+
+		public override void Enter(GameObject obj)
+		{
+			base.Enter(obj);
+			cryoEvent.audioPlayer.Play (voiceLinesTrapped);
+			cryoDoor.open = false;
+		}
+
+		public override void Update(GameObject obj)
+		{
+			 if (cryoChair.IsActive())
+			{
+				BlackBoardProperty checkValue = BlackBoard.Me().GetPropertyByName (testProperty.name);
+
+				if (checkValue.Compare (testProperty))
+					room.EnterEndingCryoGood();
+				else
+					room.EnterEndingCryoBad();
+
+				cryoEvent.EnterState(cryoEvent.idleState);
+			}
+		}
+	}
+
+	public BaseState idleState = new BaseState();
+	public Stage1 stage1 = new Stage1();
+	public Stage2 stage2 = new Stage2();
 	AudioPlayer audioPlayer;
-	int currentIndex;
-	bool active = false;
 	bool playerInCryoRoom = false;
 
 	public void Activate()
 	{
-		active = true;
-		playerInCryoRoom = false;
-		currentIndex = 0;
-		cryoDoor.open = true;
-		chatTerminal.SetLocked(true);
-		closeTerminal.Raise();
+		if (currentState == idleState)
+		{
+			EnterState (stage1);
+			playerInCryoRoom = false;
+		}
 	}
 
 	public void SetPlayerInCryoRoom()
@@ -37,37 +110,8 @@ class CryoDay3Event : MonoBehaviour
 
 	private void Start()
 	{
-		active = false;
+		EnterState (idleState);
 		audioPlayer = GetComponent <AudioPlayer>();
-		currentIndex = 0;
-	}
-
-	private void Update()
-	{
-		if (active)
-		{
-			if (playerInCryoRoom && cryoDoor.open)
-			{
-				Debug.Log("close door");
-				audioPlayer.Play (voiceLinesTrapped);
-				cryoDoor.open = false;
-				chatTerminal.SetLocked(false);
-			}
-			else if (playerInCryoRoom && cryoChair.IsActive())
-			{
-				active = false;
-				chatTerminal.SetLocked(false);
-
-				BlackBoardProperty checkValue = BlackBoard.Me().GetPropertyByName (testProperty.name);
-
-				if (checkValue.Compare (testProperty))
-					room.EnterEndingCryoGood();
-				else
-					room.EnterEndingCryoBad();
-			}
-			else if (!playerInCryoRoom && !audioPlayer.IsPlaying())
-				audioPlayer.Play(voiceLines[currentIndex++]);
-		}
 	}
 }
 
