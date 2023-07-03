4fa1664c15ada5ba048e73c19b8c63ab73e2e35d
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-07-03 15:44:53 +0200

    NEW GraphicSettings

--- Assets/Scripts/Settings/GraphicSettings.cs
+++ Assets/Scripts/Settings/GraphicSettings.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class GraphicSettings : ISettingContainer
 {
 	public bool fullscreen = true;
+	public Resolution resolution;
 
 	public GraphicSettings() { LoadSettings();}
 
 	public void SaveSettings()
 	{
+		Debug.Log ("SaveSettings");
 		PlayerPrefs.SetInt ("IvyyyFullscreen", fullscreen ? 1 : 0);
+		PlayerPrefs.SetInt ("IvyyyresolutionW", resolution.width);
+		PlayerPrefs.SetInt ("IvyyyresolutionH", resolution.height);
+		PlayerPrefs.SetInt ("IvyyyresolutionHz", resolution.refreshRate);
 	}
 
 	public void LoadSettings()
 	{
+		Debug.Log ("LoadSettings");
 		fullscreen = PlayerPrefs.GetInt("IvyyyFullscreen") > 0 ? true : false;
+		resolution.width = PlayerPrefs.GetInt("IvyyyresolutionW");
+		resolution.width = PlayerPrefs.GetInt("IvyyyresolutionH");
+		resolution.width = PlayerPrefs.GetInt("IvyyyresolutionHz");
+
+		if (resolution.width == 0 || resolution.height == 0)
+			resolution = Screen.currentResolution;
 	}
 }
--- Assets/Scripts/UI/GraphicsSettingsUi.cs
+++ Assets/Scripts/UI/GraphicsSettingsUi.cs
@@ -3,22 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class GraphicsSettingsUi : MonoBehaviour
 {
 	[SerializeField] SwitchToggle fullscreen;
-	//[SerializeField] Dropdown resolution;
+	[SerializeField] TMP_Dropdown resolutionDropDown;
 
+	Resolution[] resolutions;
 	GraphicSettings graphicSettings;
+	int currentIndex;
 
 	private void OnEnable()
 	{
+		currentIndex = -1;
+
+		resolutions = Screen.resolutions;
 		graphicSettings = GameSettings.Me().graphicSettings;
 		fullscreen.active = graphicSettings.fullscreen;
-		//resolution.options.Clear();
 
-		//foreach (var i in Screen.resolutions)
-		//	resolution.options.Add (new Dropdown.OptionData (i.ToString()));
+		resolutionDropDown.options.Clear();
+
+		for (int i = 0; i < resolutions.Length; ++i)
+		{
+			Resolution tmp = resolutions[i];
+			resolutionDropDown.options.Add(new TMP_Dropdown.OptionData(tmp.ToString()));
+		}
+
+		int index = Array.IndexOf (resolutions, graphicSettings.resolution);
+		
+		if (index != -1)
+		{
+			Debug.Log ("Saved res");
+			resolutionDropDown.value = index;
+			currentIndex = index;
+		}
+		else
+		{
+			Debug.Log ("Default res");
+			resolutionDropDown.value = resolutions.Length -1;
+		}
+	}
+
+	private void Update()
+	{
+		if (fullscreen.active != graphicSettings.fullscreen)
+			graphicSettings.fullscreen = fullscreen.active;
+
+		if (resolutionDropDown.value != currentIndex)
+		{
+			currentIndex = resolutionDropDown.value;
+			Resolution tmp = resolutions[currentIndex];
+			Screen.SetResolution (tmp.width, tmp.height, graphicSettings.fullscreen, tmp.refreshRate);
+		}
 	}
 
 	private void OnDisable()
@@ -34,6 +71,7 @@ public class GraphicsSettingsUi : MonoBehaviour
 	public void Save()
 	{
 		graphicSettings.fullscreen = fullscreen.active;
+		graphicSettings.resolution = resolutions[currentIndex];
 		graphicSettings.SaveSettings();
 	}
 }
